2f007f0901c18e8b8000cdade02bf17b51e98d4c
MuntasirBabul <muntasirbabul9@gmail.com>
2022-07-13 13:41:35 +0600

    still developing

--- DUAL_PORT_RAM/tb_dual_port_ram.sv
+++ DUAL_PORT_RAM/tb_dual_port_ram.sv
@@ -9,65 +9,65 @@ module tb_dual_port_ram #
 )
 ();
 
-logic clk_t;
-logic [BUS_WIDTH-1:0] data_in_A_t;    // BUS_IN                     > port A
-logic [BUS_WIDTH-1:0] data_in_B_t;    // BUS_IN                     > port B
-logic [BUS_WIDTH-1:0] data_out_A_t;   // BUS_OUT                    > port A
-logic [BUS_WIDTH-1:0] data_out_B_t;   // BUS_OUT                    > port B
+logic clk;
+logic [BUS_WIDTH-1:0] data_in_A;    
+logic [BUS_WIDTH-1:0] data_in_B;    
+logic [BUS_WIDTH-1:0] data_out_A;   
+logic [BUS_WIDTH-1:0] data_out_B;   
 
-logic [ADDR_WIDTH-1:0] addr_A_t;      // ADDRESS                    > port A
-logic [ADDR_WIDTH-1:0] addr_B_t;      // ADDRESS                    > port B
+logic [ADDR_WIDTH-1:0] addr_A;      
+logic [ADDR_WIDTH-1:0] addr_B;      
 
-logic ready_r_A_t;                      // READY::READ                > port A    
-logic valid_r_A_t;                      // VALID::READ                > port A
-logic ready_w_A_t;                      // READY::WRITE               > port A
-logic valid_w_A_t;                      // VALID::WRITE               > port A
+logic ready_r_A;                    
+logic valid_r_A;                    
+logic ready_w_A;                    
+logic valid_w_A;                    
 
-logic ready_r_B_t;                      // READY::READ                > port B    
-logic valid_r_B_t;                      // VALID::READ                > port B
-logic ready_w_B_t;                      // READY::WRITE               > port B
-logic valid_w_B_t;                      // VALID::WRITE               > port B
+logic ready_r_B;                      
+logic valid_r_B;                      
+logic ready_w_B;                      
+logic valid_w_B;                      
 
-logic addr_ready_A_t;                   // READY::ADDRESS             > port A
-logic addr_valid_A_t;                   // VALID::ADDRESS             > port A
-logic addr_ready_B_t;                   // READY::ADDRESS             > port B
-logic addr_valid_B_t;                   // VALID::ADDRESS             > port B
+logic addr_ready_A;                   
+logic addr_valid_A;                   
+logic addr_ready_B;                   
+logic addr_valid_B;                   
 
-logic we_A_t;                           // WRITE_ENABLE               > port A
-logic we_B_t;                           // WRITE_ENABLE               > port B
-logic en_t;              
+logic we_A;                           
+logic we_B;                           
+logic en;              
 event ready_edge_A, ready_edge_B, clk_edge;
 
 dual_port_ram_64bit DUT
 (
-.clk            (clk_t)         ,
-.data_in_A      (data_in_A_t)   ,
-.data_in_B      (data_in_B_t)   ,
-.data_out_A     (data_out_A_t)  ,
-.data_out_B     (data_out_B_t)  ,
-.addr_A         (addr_A_t)      ,
-.addr_B         (addr_B_t)      ,
-.ready_r_A      (ready_r_A_t)   ,
-.valid_r_A      (valid_r_A_t)   ,
-.ready_r_B      (ready_r_B_t)   ,
-.valid_r_B      (valid_r_B_t)   ,
-.ready_w_A      (ready_w_A_t)   ,
-.valid_w_A      (valid_w_A_t)   ,
-.ready_w_B      (ready_w_B_t)   ,
-.valid_w_B      (valid_w_B_t)   ,
-.addr_ready_A   (addr_ready_A_t),
-.addr_valid_A   (addr_valid_A_t),
-.addr_ready_B   (addr_ready_B_t),
-.addr_valid_B   (addr_valid_B_t),
-.we_A           (we_A_t)        ,
-.we_B           (we_B_t)        ,
-.en             (en_t)
+.clk            (clk)         ,
+.data_in_A      (data_in_A)   ,
+.data_in_B      (data_in_B)   ,
+.data_out_A     (data_out_A)  ,
+.data_out_B     (data_out_B)  ,
+.addr_A         (addr_A)      ,
+.addr_B         (addr_B)      ,
+.ready_r_A      (ready_r_A)   ,
+.valid_r_A      (valid_r_A)   ,
+.ready_r_B      (ready_r_B)   ,
+.valid_r_B      (valid_r_B)   ,
+.ready_w_A      (ready_w_A)   ,
+.valid_w_A      (valid_w_A)   ,
+.ready_w_B      (ready_w_B)   ,
+.valid_w_B      (valid_w_B)   ,
+.addr_ready_A   (addr_ready_A),
+.addr_valid_A   (addr_valid_A),
+.addr_ready_B   (addr_ready_B),
+.addr_valid_B   (addr_valid_B),
+.we_A           (we_A)        ,
+.we_B           (we_B)        ,
+.en             (en)
 );
 
 initial begin 
-    clk_t = 0;
+    clk = 0;
 forever 
-    #(CYCLE/2) clk_t = ~clk_t;   //5+5 
+    #(CYCLE/2) clk = ~clk;   
 end
 
 class addr_gen_data;
@@ -76,7 +76,8 @@ class addr_gen_data;
     bit [BUS_WIDTH-1:0] temp[0:9];
     integer i = 0;
 
-    constraint data_addr_const{
+    constraint data_addr_const
+    {
         addr <= 429496729;
         data <= 64'hFFFF_FFFF_FFFF_FFFF;
     }
@@ -85,26 +86,26 @@ function void post_randomize();
     temp[i++] = addr;
 endfunction
 
-function void assign_port_A;
-    addr_A_t    = addr;
-    data_in_A_t = data;
+function void assign_port_A();
+    addr_A    = addr;
+    data_in_A = data;
 endfunction
 
-function void assign_port_B;
-    addr_B_t    = addr;
-    data_in_B_t = data;
+function void assign_port_B();
+    addr_B    = addr;
+    data_in_B = data;
 endfunction
 endclass
 
-always@(negedge ready_w_A_t) begin
+always@(negedge ready_w_A) begin
     -> ready_edge_A;
 end
 
-always@(negedge ready_w_B_t) begin
+always@(negedge ready_w_B) begin
     -> ready_edge_B;
 end
 
-always@(posedge clk_t) begin
+always@(posedge clk) begin
     -> clk_edge;
 end
 
@@ -113,14 +114,14 @@ initial begin
     addr_gen_data test_B = new();
     integer j = 0;
     integer k = 0;
-    ready_r_A_t = 1;
-    ready_r_B_t = 1;
-    valid_w_B_t = 1;
-    addr_valid_B_t =1; 
+    ready_r_A = 1;
+    ready_r_B = 1;
+    valid_w_B = 1;
+    addr_valid_B =1; 
 
 #5
 
-$monitor("addr A=%h, data_out_A=%h\t, addrB=%h, data_out_B=%h, time=%0t\n",addr_A_t,data_out_A_t,addr_B_t,data_out_B_t,$time);
+$monitor("addr A=%h, data_out_A=%h\t, addrB=%h, data_out_B=%h, time=%0t\n",addr_A,data_out_A,addr_B,data_out_B,$time);
 
 fork
     repeat(10) begin 
@@ -137,32 +138,32 @@ fork
     end
 join
 
-    addr_valid_A_t  = 1;
-    data_in_A_t     = 'z;
-    addr_A_t        = 'z;
-    valid_w_A_t     = 0;
-    we_A_t          = 0;
+    addr_valid_A  = 1;
+    data_in_A     = 'z;
+    addr_A        = 'z;
+    valid_w_A     = 1;
+    we_A          = 1;
 
-    addr_valid_B_t  = 1;
-    data_in_B_t     = 'z;
-    addr_B_t        = 'z;
-    valid_w_B_t     = 0;
-    we_B_t          = 0;
+    addr_valid_B  = 1;
+    data_in_B     = 'z;
+    addr_B        = 'z;
+    valid_w_B     = 1;
+    we_B          = 1;
 
 fork
     repeat(10) begin 
-        addr_A_t = test_A.temp[j++];
+        addr_A = test_A.temp[j++];
         #5;
     end
 
     repeat(10) begin
-        addr_B_t = test_B.temp[k++];
+        addr_B = test_B.temp[k++];
         #5;
     end
 join
 
-    addr_valid_A_t = 0;
-    addr_valid_B_t = 0;
+    addr_valid_A = 0;
+    addr_valid_B = 0;
     #10;
     $finish;
 end  
