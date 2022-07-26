bf0546526d1dfdb91ba7ab343027c12a847faef7
MuntasirBabul <muntasirbabul9@gmail.com>
2022-07-26 13:39:54 +0600

    added agent pkg

---
+++ DUAL_PORT_RAM_UVM/verif/env/agents/ram_agent_pkg.sv
@@ -0,0 +1,16 @@
+`ifndef RAM_AGENT_PKG
+`define RAM_AGENT_PKG
+
+package ram_agent_pkg;
+`include "uvm_macros.svh"
+`include "ram_defines.svh"
+`include "ram_seq_item.sv"
+`include "ram_sequencer_0.sv"
+`include "ram_sequencer_1.sv"
+`include "ram_driver_0.sv"
+`include "ram_driver_1.sv"
+`include "ram_monitor_0.sv"
+`include "ram_monitor_1.sv"
+`include "ram_agent_0.sv"
+`include "ram_agent_1.sv"
+endpackage
