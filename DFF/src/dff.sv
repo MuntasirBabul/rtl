db8296827c7c5bb950833954223e51528716d304
MuntasirBabul <muntasirbabul9@gmail.com>
2022-08-29 14:46:13 +0600

    added cocotb test and coverage

--- DFF/src/dff.sv
+++ DFF/src/dff.sv
@@ -1,14 +1,10 @@
 module DFF
 (
 	input logic d,
-	input logic a_rst,
 	input logic clk,
 	output logic q
 );
 
-always@(posedge clk or negedge a_rst)
-	if(!a_rst)
-		q <= 0;
-	else q <= d;
-
+always@(posedge clk) begin
+	 		q <= d; end
 endmodule
