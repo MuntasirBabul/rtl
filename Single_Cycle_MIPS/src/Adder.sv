f727d44158da8a68c11379c47cc2c59a85e762ed
muntasirbhai <muntasirbabul9@gmail.com>
2022-08-31 03:49:27 +0600

    added adder

---
+++ Single_Cycle_MIPS/src/Adder.sv
@@ -0,0 +1,13 @@
+module Adder #
+(
+    parameter WIDTH = 32
+)
+(
+    input logic [WIDTH-1:0]     A,
+    input logic [WIDTH-1:0]     B,
+    output logic [WIDTH-1:0]    C
+ )
+
+ assign C = A + B;
+
+endmodule
