7043fcd06b8ed0cbad2a6035204749713537ed09
muntasirbhai <muntasirbabul9@gmail.com>
2022-11-25 19:27:41 +0600

    added test list

---
+++ Dual_Port_RAM/DUAL_PORT_RAM_UVM/verif/tests/src/ram_test_list.sv
@@ -0,0 +1,15 @@
+`ifndef RAM_TEST_LIST
+`define RAM_TEST_LIST
+
+package ram_test_list;
+
+import uvm_pkg::*
+`include "uvm_macros.svh"
+
+import ram_env_pkg::*
+import ram_seq_list::*
+
+`include "ram_test.sv"
+
+endpackage
+`endif
