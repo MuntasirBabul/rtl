b9ebec4ad538e1abfc0c69faddb578842b12cf29
muntasirbhai <muntasirbabul9@gmail.com>
2022-11-25 19:11:44 +0600

    updated syntax errors

--- Dual_Port_RAM/DUAL_PORT_RAM_UVM/verif/tb/ram_interface.sv
+++ Dual_Port_RAM/DUAL_PORT_RAM_UVM/verif/tb/ram_interface.sv
@@ -10,128 +10,128 @@ interface ram_if #
     input logic aclk, aresetn	
 );
 
-    logic [BUS_WIDTH-1:0] data_in_A,    // BUS_IN                     > port A
-    logic [BUS_WIDTH-1:0] data_in_B,    // BUS_IN                     > port B
-    logic [BUS_WIDTH-1:0] data_out_A,   // BUS_OUT                    > port A
-    logic [BUS_WIDTH-1:0] data_out_B,   // BUS_OUT                    > port B
+    logic [BUS_WIDTH-1:0] data_in_A;    // BUS_IN                     > port A
+    logic [BUS_WIDTH-1:0] data_in_B;    // BUS_IN                     > port B
+    logic [BUS_WIDTH-1:0] data_out_A;   // BUS_OUT                    > port A
+    logic [BUS_WIDTH-1:0] data_out_B;   // BUS_OUT                    > port B
 
-    logic [ADDR_WIDTH-1:0] addr_A,      // ADDRESS                    > port A
-    logic [ADDR_WIDTH-1:0] addr_B,      // ADDRESS                    > port B
+    logic [ADDR_WIDTH-1:0] addr_A;      // ADDRESS                    > port A
+    logic [ADDR_WIDTH-1:0] addr_B;      // ADDRESS                    > port B
 
-    bit ready_r_A,                      // READY::READ                > port A
-    bit valid_r_A,                      // VALID::READ                > port A
-    bit ready_w_A,                      // READY::WRITE               > port A
-    bit valid_w_A,                      // VALID::WRITE               > port A
+    bit ready_r_A;                      // READY::READ                > port A
+    bit valid_r_A;                      // VALID::READ                > port A
+    bit ready_w_A;                      // READY::WRITE               > port A
+    bit valid_w_A;                      // VALID::WRITE               > port A
 
-    bit ready_r_B,                      // READY::READ                > port B
-    bit valid_r_B,                      // VALID::READ                > port B
-    bit ready_w_B,                      // READY::WRITE               > port B
-    bit valid_w_B,                      // VALID::WRITE               > port B
+    bit ready_r_B;                      // READY::READ                > port B
+    bit valid_r_B;                      // VALID::READ                > port B
+    bit ready_w_B;                      // READY::WRITE               > port B
+    bit valid_w_B;                      // VALID::WRITE               > port B
 
-    bit addr_ready_A,                   // READY::ADDRESS             > port A
-    bit addr_valid_A,                   // VALID::ADDRESS             > port A
-    bit addr_ready_B,                   // READY::ADDRESS             > port B
-    bit addr_valid_B,                   // VALID::ADDRESS             > port B
+    bit addr_ready_A;                   // READY::ADDRESS             > port A
+    bit addr_valid_A;                   // VALID::ADDRESS             > port A
+    bit addr_ready_B;                   // READY::ADDRESS             > port B
+    bit addr_valid_B;                   // VALID::ADDRESS             > port B
 
-    bit we_A,                           // WRITE_ENABLE               > port A
-    bit we_B,                           // WRITE_ENABLE               > port B
+    bit we_A;                           // WRITE_ENABLE               > port A
+    bit we_B;                           // WRITE_ENABLE               > port B
     bit en                              // ASYNC_ENABLE and SYNC_WRITE_ENABLE
 
 modport DUT
 (
-    input   logic aclk,                                   
+    input    aclk,                                   
 
-    input   logic [BUS_WIDTH-1:0] data_in_A,                                           
-    input   logic [BUS_WIDTH-1:0] data_in_B,                                            
-    output  logic [BUS_WIDTH-1:0] data_out_A,                                         
-    output  logic [BUS_WIDTH-1:0] data_out_B,                                         
+    input    data_in_A,                                           
+    input    data_in_B,                                            
+    output   data_out_A,                                         
+    output   data_out_B,                                         
 
-    input   logic [ADDR_WIDTH-1:0] addr_A,                                             
-    input   logic [ADDR_WIDTH-1:0] addr_B,                                            
+    input    addr_A,                                             
+    input    addr_B,                                            
 
-    input   bit ready_r_A,                                                                
-    output  bit valid_r_A,                                                            
-    output  bit ready_w_A,                                                            
-    input   bit valid_w_A,
+    input    ready_r_A,                                                                
+    output   valid_r_A,                                                            
+    output   ready_w_A,                                                            
+    input    valid_w_A,
 
-	input   bit ready_r_B,                                                                
-    output  bit valid_r_B,                                                             
-    output  bit ready_w_B,                                                             
-    input   bit valid_w_B,                                                            
+	input    ready_r_B,                                                                
+    output   valid_r_B,                                                             
+    output   ready_w_B,                                                             
+    input    valid_w_B,                                                            
 
-    output  bit addr_ready_A,                                                         
-    input   bit addr_valid_A,                                                         
-    output  bit addr_ready_B,                                                         
-    input   bit addr_valid_B,                                                         
+    output   addr_ready_A,                                                         
+    input    addr_valid_A,                                                         
+    output   addr_ready_B,                                                         
+    input    addr_valid_B,                                                         
 
-    input   bit we_A,                                                                 
-    input   bit we_B,                                                                 
-    input   bit en                                                                     
+    input    we_A,                                                                 
+    input    we_B,                                                                 
+    input    en                                                                     
 );
 modport monitor
 (
-    input   logic aclk,                                  
+    input    aclk,                                  
 
-    input   logic [BUS_WIDTH-1:0] data_in_A,                                          
-    input   logic [BUS_WIDTH-1:0] data_in_B,                                          
-    input   logic [BUS_WIDTH-1:0] data_out_A,                                         
-    input   logic [BUS_WIDTH-1:0] data_out_B,                                         
+    input    data_in_A,                                          
+    input    data_in_B,                                          
+    input    data_out_A,                                         
+    input    data_out_B,                                         
 
-    input   logic [ADDR_WIDTH-1:0] addr_A,
-    input   logic [ADDR_WIDTH-1:0] addr_B,
+    input    addr_A,
+    input    addr_B,
 
-    input   bit ready_r_A,
-    input   bit valid_r_A,
-    input   bit ready_w_A,
-    input   bit valid_w_A,
+    input    ready_r_A,
+    input    valid_r_A,
+    input    ready_w_A,
+    input    valid_w_A,
 	
-	input   bit ready_r_B,
-    input   bit valid_r_B,
-    input   bit ready_w_B,
-    input   bit valid_w_B,
+	input    ready_r_B,
+    input    valid_r_B,
+    input    ready_w_B,
+    input    valid_w_B,
 
-    input   bit addr_ready_A,
-    input   bit addr_valid_A,
-    input   bit addr_ready_B,
-    input   bit addr_valid_B,
+    input    addr_ready_A,
+    input    addr_valid_A,
+    input    addr_ready_B,
+    input    addr_valid_B,
 
-    input   bit we_A,
-    input   bit we_B,
-    input   bit en
+    input    we_A,
+    input    we_B,
+    input    en
 );
 modport driver_0
 (	
-	input   logic aclk,
-	output  logic [BUS_WIDTH-1:0] data_in_A,
-	input   logic [BUS_WIDTH-1:0] data_out_A,
-	output  logic [ADDR_WIDTH-1:0] addr_A,
+	input    aclk,
+	output   data_in_A,
+	input    data_out_A,
+	output   addr_A,
 	
-	output  bit ready_r_A,
-	input   bit valid_r_A,
-	input   bit ready_w_A,
-	output  bit valid_w_A,
+	output   ready_r_A,
+	input    valid_r_A,
+	input    ready_w_A,
+	output   valid_w_A,
 	
-	input   bit addr_ready_A,
-	output  bit addr_valid_A,
-	output  bit we_A,
-	output  bit en
+	input    addr_ready_A,
+	output   addr_valid_A,
+	output   we_A,
+	output   en
 );
 modport driver_1
 (
-    input   logic aclk,
-    output  logic [BUS_WIDTH-1:0] data_in_B,
-    input   logic [BUS_WIDTH-1:0] data_out_B,
-    output  logic [ADDR_WIDTH-1:0] addr_B,
+    input    aclk,
+    output   data_in_B,
+    input    data_out_B,
+    output   addr_B,
 
-    output  bit ready_r_B,
-    input   bit valid_r_B,
-    input   bit ready_w_B,
-    output  bit valid_w_B,
+    output   ready_r_B,
+    input    valid_r_B,
+    input    ready_w_B,
+    output   valid_w_B,
 
-    input   bit addr_ready_B,
-    output  bit addr_valid_B,
-    output  bit we_B,
-    output  bit en
+    input    addr_ready_B,
+    output   addr_valid_B,
+    output   we_B,
+    output   en
 );
 
 endinterface
